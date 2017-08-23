cae98043199669614ac2559c8170d4eb11f8587d
王健 <wangjian@jeuci.com>
2017-08-23 11:49:13 +0800

    +

--- src/pages/wo.vue
+++ src/pages/wo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="maincontainer">
     <div class="userinfo">
-      <img class="userinfo-avatar" src="" background-size="cover"></img>
+      <img class="userinfo-avatar" src="../../static/images/me/User-104.png" background-size="cover"></img>
       <span class="userinfo-nickname">nickname</span>
     </div>
 
@@ -9,35 +9,35 @@
     </div>
 
     <div class="bottom">
-      <div class="bottom-cell">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}">
         <img src="../../static/images/me/User-05.png"></img>
         <span>购买授权</span>
       </div>
-      <div class="bottom-cell">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}">
         <img src="../../static/images/me/User-07.png"></img>
         <span>在线客服</span>
       </div>
-      <div class="bottom-cell" bindtap="shouquanClick">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}" bindtap="shouquanClick">
         <img src="../../static/images/me/User-08.png"></img>
         <span>我的授权</span>
       </div>
-      <div class="bottom-cell" bindtap="XGpwdClick">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}" bindtap="XGpwdClick">
         <img src="../../static/images/me/User-09.png"></img>
         <span>密码管理</span>
       </div>
-      <div class="bottom-cell" bindtap="feedbackClick">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}" bindtap="feedbackClick">
         <img src="../../static/images/me/User-10.png"></img>
         <span>意见反馈</span>
       </div>
-      <div class="bottom-cell" bindtap="aboutClick">
+      <div class="bottom-cell" :style="{width:ojwidth+'px',height:ojwidth+'px'}" bindtap="aboutClick">
         <img src="../../static/images/me/User-11.png"></img>
         <span>关于我们</span>
       </div>
     </div>
 
-    <!-- <div class="bottom-btndiv">
-        <button plain="{{true}}" style="background-color: rgb(229, 87, 77);border-color:rgba(0,0,0,0);color:#fff;" class="bottom-btn">退出登录</button>
-      </div> -->
+    <div class="bottom-btndiv">
+      <button class="bottom-btn">退出登录</button>
+    </div>
 
   </div>
 </template>
@@ -51,16 +51,16 @@
 }
 
 .userinfo-avatar {
-  width: 128px;
-  height: 128px;
-  margin: 20px;
+  width: 64px;
+  height: 64px;
+  margin: 10px;
   border-radius: 50%;
 }
 
 .userinfo-nickname {
   color: #fff;
-  font-size: 24px;
-  padding-bottom: 20rpx;
+  font-size: 18px;
+  padding-bottom: 10px;
 }
 
 .middle {
@@ -75,24 +75,24 @@
 }
 
 .bottom-cell {
-  width: 125px;
-  height: 125px;
-  border: 1px solid gray;
-  // border-style: none solid solid none;
-  display: flex;
+  width: 124px;
+  height: 124px;
+  border: 1px solid gray; // border-style: none solid solid none;
   flex-direction: column;
+  display: -webkit-flex;
+  -webkit-align-items: center;
+  align-items: center;
+  -webkit-justify-content: center;
+  justify-content: center;
 }
 
-.bottom-cell image {
+.bottom-cell img {
   width: 50px;
   height: 50px;
-  margin-top: 25px;
-  margin-left: 39px;
 }
 
 .bottom-cell text {
   text-align: center;
-  margin-top: 10px;
   font-size: 26px;
   color: gray;
 }
@@ -107,10 +107,69 @@
 }
 
 .bottom-btn {
+  margin-top: 40px;
+  margin-bottom: 20px;
   width: 50%;
   height: 40px;
   border-radius: 20px;
-  line-height: 40px;
+  line-height: 35px;
+  background-color: rgb(229, 87, 77);
+  border-color: rgba(0, 0, 0, 0);
+  color: #fff;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      screenWidth: document.body.clientWidth,
+    }
+  },
+  mounted() {
+    const that = this
+    window.onresize = () => {
+      return (() => {
+        window.screenWidth = document.body.clientWidth
+        that.screenWidth = window.screenWidth
+      })()
+    }
+
+  },
+  watch: {
+    screenWidth(val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        this.timer = true
+        let that = this
+        setTimeout(function() {
+          // that.screenWidth = that.$store.state.canvasWidth
+          // that.init()
+          that.timer = false
+        }, 400)
+      }
+    }
+  },
+  computed: {
+    ojwidth: {
+      // getter
+      get: function() {
+        var ojwidth;
+        var margin;
+        var colnum = 3;//列
+        colnum = 6 > colnum ? colnum : 6;
+        var rownum = 6 / colnum;//行
+        margin = 6 > 5 ? 1 : 1;//间距
+        ojwidth = (this.screenWidth - (2 * (margin + 0) * colnum)) / colnum;//格子的宽
+        return ojwidth;
+      },
+      // setter
+      set: function(newValue) {
+
+      }
+    }
+  }
+}
+</script>
+
 
