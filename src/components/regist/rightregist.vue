fb715953e8ec374e070643bfc8861d266de5e85c
王健 <wangjian@jeuci.com>
2017-08-24 14:04:15 +0800

    +

---
+++ src/components/regist/rightregist.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      rightregist
+  </div>
+</template>
--- src/pages/regist.vue
+++ src/pages/regist.vue
@@ -1,24 +1,50 @@
 <template>
   <div>
     <m-header :title="title"></m-header>
-    regist
+    <div class="btnview">
+      <el-button class="btn" plain=true type="primary">手机注册</el-button>
+      <el-button class="btn" plain=true type="primary">用户名注册</el-button>
+    </div>
   </div>
 </template>
 
+<style lang="less" scoped>
+.btnview {
+  width: 100%;
+  height: 10%;
+  display: flex;
+  flex-direction: row;
+  .btn {
+    left: 0;right: 0;
+    width: 50%;
+    height: 100%;
+    background-color: RGB(251, 230, 231);
+    border: 0;
+    color: red;
+    font-size: 18px;
+  }
+}
+</style>
+
+
 <script>
 import mHeader from '../components/hearder/Hearder';
+import leftRegist from '../components/regist/leftregist'
+import rightRegist from '../components/regist/rightregist'
+
 export default {
   name: 'registVC',
   data() {
     return {
       title: {
         text: "注册",
-        showBack: true
+        showBack: true,
       },
     };
   },
   components: {
     mHeader
   },
+
 }
 </script>
