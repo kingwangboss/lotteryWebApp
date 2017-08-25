96388c4a3494d5fa7168c3fd6eddb1be050c48ea
OneJ <3942241@qq.com>
2017-08-25 18:58:22 +0800

    +

--- src/pages/regist.vue
+++ src/pages/regist.vue
@@ -26,6 +26,8 @@
     height: 100%;
     background-color: RGB(251, 230, 231);
     background-image: url(../../static/images/login/Register-03.png);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
     border: 0;
     color: #B54123;
     font-size: 15px;
@@ -38,6 +40,8 @@
     height: 100%;
     background-color: RGB(251, 230, 231);
     background-image: url(../../static/images/login/Register-02.png);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
     border: 0;
     color:white;
     font-size: 15px;
