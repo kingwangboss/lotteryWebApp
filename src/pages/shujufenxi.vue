c1b231dfc1ce24a0dd3a0890c9fb398765a4911d
OneJ <3942241@qq.com>
2017-09-13 18:30:38 +0800

    +

--- src/pages/shujufenxi.vue
+++ src/pages/shujufenxi.vue
@@ -2,10 +2,10 @@
   <div class="main">
     <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
       <el-tab-pane label="遗漏分析" name="first">
-        <iframe class="ichart" name="txt0" :style="{height:screenHeight+'px'}" src="../../static/JYYC1/txt.html" frameborder="0" marginheight="0px"></iframe>
+        <iframe class="ichart" name="txt0" :style="{height:screenHeight+'px'}" src="https://ycws1.cpnet.com/chart/txt.html" frameborder="0" marginheight="0px"></iframe>
       </el-tab-pane>
       <el-tab-pane label="冷热分析" name="second">
-        <iframe class="ichart" :style="{height:screenHeight+'px'}" name="bxt0" src="../../static/JYYC1/bxt.html" frameborder="0" marginheight="0px"></iframe>
+        <iframe class="ichart" :style="{height:screenHeight+'px'}" name="bxt0" src="http://project.camew.com:8020" frameborder="0" marginheight="0px"></iframe>
       </el-tab-pane>
       <el-tab-pane label="指标遗漏分析" name="third">
         <iframe class="ichart" :style="{height:screenHeight+'px'}" name="txt1" src="../../static/JYYC1/txt.html" frameborder="0" marginheight="0px"></iframe>
