5075a37740d4c51941753e63500a818058449792
OneJ <3942241@qq.com>
2018-06-06 16:34:08 +0800

    +

--- src/pages/wo/feeback.vue
+++ src/pages/wo/feeback.vue
@@ -99,7 +99,7 @@ export default {
       data.append("Sign", sha256.sha256(signStr).toUpperCase());
 
       this.$http
-        .post(localStorage.SiteUrl, data)
+        .post(this.global.url, data)
         .then(res => {
           // this.$router.go(-1);
         })
